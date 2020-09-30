6b3e98c11d4cda55585c1c9972a638ab3a92e51c
José Benjumeda <jbenjumeda@afi.es>
2020-09-30 11:24:57 +0200

    Punto 5 del tutorial terminado

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -12,20 +12,9 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   /*Asi se declara una variable inicializada a HEROES, y supongo que angular sabe el tipo, que sera el mismo de HEROES.*/
   heroes: Hero[];
-  /*Asi se declara una variable del tipo Hero sin inicializar.*/
-  selectedHero: Hero;
+  
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
-
-  /*Esto es una funcion declarada. Dentro de los parentesis se declaran los parametros como variables normales. Luego se
-   pone el tipo de lo que devuelve la funcion y luego se empieza el codigo de la misma. */
-  onSelect(hero: Hero): void {
-
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-
-
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
